be6680689fe60d48f3d89abd41b438bfe270a71a
jabedzaman <jabedzaman2004@gmail.com>
2023-02-22 00:05:14 +0530

    add instal script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!bin/bash/
+
+if ! command -v rustc &> /dev/null
+then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
+
+cargo install --path .
+
+echo "Done!"
+echo "warning: you may need to add ~/.cargo/bin to your PATH variable."
+echo "restart your terminal and run 'easygit."
